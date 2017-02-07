370e8ee7f695e9320ab80800b2b30a048c8e76f1
Luis Ramón López <lrlopez@gmail.com>
2017-02-07 14:33:32 +0100

    Preparado listado de grupos para editar

--- src/AppBundle/Controller/GrupoController.php
+++ src/AppBundle/Controller/GrupoController.php
@@ -2,9 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Grupo;
 use Doctrine\ORM\EntityManager;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class GrupoController extends Controller
 {
@@ -27,6 +29,19 @@ class GrupoController extends Controller
 
         return $this->render('grupo/listar.html.twig', [
             'grupos' => $grupos
+        ]);
+    }
+
+    /**
+     * @Route("/grupos/modificar/{descripcion}", name="modificar_grupo")
+     */
+    public function formGrupoAction(Request $request, Grupo $grupo)
+    {
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+
+        return $this->render('grupo/form.html.twig', [
+            'grupo' => $grupo
         ]);
     }
 }
