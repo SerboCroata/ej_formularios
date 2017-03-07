982927cf4653e9d726b6a8857cc042862ad6d4e4
Luis Ramón López <lrlopez@gmail.com>
2017-03-07 13:50:30 +0100

    Añadido formulario de entrada y rutas de salida

---
+++ src/AppBundle/Controller/UsuarioController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class UsuarioController extends Controller
+{
+    /**
+     * @Route("/entrar", name="entrar")
+     */
+    public function indexAction()
+    {
+        // replace this example code with whatever you need
+        return $this->render('usuario/login.html.twig');
+    }
+
+    /**
+     * @Route("/comprobar", name="comprobar")
+     * @Route("/salir", name="salir")
+     */
+    public function comprobarAction() {
+    }
+}
